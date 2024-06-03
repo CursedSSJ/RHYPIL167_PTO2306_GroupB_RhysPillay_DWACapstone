9cc6ed0a1c5c94fbcfba1b75d47d2a07507c765e
Rhys Pillay <rhys.pillay@gmail.com>
2024-06-03 16:03:49 +0200

    Added Carasoul and styling

--- src/feature/content/components/infoCard.jsx
+++ src/feature/content/components/infoCard.jsx
@@ -1,22 +1,32 @@
 import React, { useState, useEffect } from "react";
+import Slider from "react-slick";
 import {
   CircularProgress,
-  Container,
   Typography,
+  Container,
   Card,
   CardContent,
   CardMedia,
   Grid,
+  Button,
+  TextField,
 } from "@mui/material";
+
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 import genres from "../config/genres";
 import { styles } from "../styles/infoCard-styles";
 import { useTheme } from "@mui/material/styles";
 
 const InfoCard = () => {
-  const [podcastData, setPodcastData] = useState(null);
+  const [podcastData, setPodcastData] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
+  const [showFullDescription, setShowFullDescription] = useState({});
+  const [filter, setFilter] = useState("");
+  const [filteredData, setFilteredData] = useState([]);
+  const [carouselData, setCarouselData] = useState([]);
 
   const theme = useTheme();
   const style = styles(theme);
@@ -30,8 +40,19 @@ const InfoCard = () => {
           throw new Error(`Error fetching podcast data: ${response.status}`);
         }
         const data = await response.json();
-        console.log(data);
         setPodcastData(data);
+        setFilteredData(data);
+
+        // Initialize the showFullDescription state for each podcast
+        const initialDescriptionState = {};
+        data.forEach((podcast) => {
+          initialDescriptionState[podcast.id] = false;
+        });
+        setShowFullDescription(initialDescriptionState);
+
+        const shuffledData = podcastData.sort(() => Math.random() - 0.5);
+        const carouselPodcasts = shuffledData.slice(0, 6);
+        setCarouselData(carouselPodcasts);
       } catch (error) {
         setError(error.message);
       } finally {
@@ -42,6 +63,14 @@ const InfoCard = () => {
     fetchData();
   }, []);
 
+  useEffect(() => {
+    setFilteredData(
+      podcastData.filter((podcast) =>
+        podcast.title.toLowerCase().includes(filter.toLowerCase())
+      )
+    );
+  }, [filter, podcastData]);
+
   if (isLoading) {
     return <CircularProgress />;
   }
@@ -50,52 +79,125 @@ const InfoCard = () => {
     return <p>Error: {error}</p>;
   }
 
+  const toggleDescription = (id) => {
+    setShowFullDescription((prev) => ({
+      ...prev,
+      [id]: !prev[id],
+    }));
+  };
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+  };
+
   return (
-    <Grid>
-      {podcastData &&
-        podcastData.map((podcast) => (
-          <Grid key={podcast.id} sx={style.gridContainer}>
-            <Card key={podcast.id}>
+    <Container sx={style.infoCardMainContainer}>
+      <Slider {...settings} style={style.infoCardCarasoulCardContainerSlider}>
+        {carouselData.map((podcast) => (
+          <Container key={podcast.id} sx={style.infoCardCarasoulCardContainer}>
+            <Card>
               <CardMedia
                 component="img"
-                height="140"
                 image={podcast.image}
                 alt={podcast.title}
               />
-              <CardContent key={podcast.id}>
-                <Grid container spacing={2}>
-                  <Grid item xs={12}>
-                    <Typography variant="h5" component="div">
-                      {podcast.title}
-                    </Typography>
-                  </Grid>
-                  <Grid item xs={12}>
-                    <Typography variant="body2" color="text.secondary">
+              <CardContent>
+                <Typography variant="h6" component="div">
+                  {podcast.title}
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  {showFullDescription[podcast.id] ? (
+                    <>
                       {podcast.description}
-                    </Typography>
-                  </Grid>
-                  <Grid item xs={6}>
-                    <Typography variant="body2" color="text.secondary">
-                      Seasons: {podcast.seasons}
-                    </Typography>
-                  </Grid>
-                  <Grid item xs={6}>
-                    <Typography variant="body2" color="text.secondary">
-                      Updated: {podcast.updated}
-                    </Typography>
-                  </Grid>
-                  <Grid item xs={12}>
-                    <Typography variant="body2" color="text.secondary">
-                      Genres:{" "}
-                      {podcast.genres.map((genre) => genres[genre]).join(", ")}
-                    </Typography>
-                  </Grid>
-                </Grid>
+                      <Button onClick={() => toggleDescription(podcast.id)}>
+                        Read Less
+                      </Button>
+                    </>
+                  ) : (
+                    <>
+                      {podcast.description.slice(0, 100)}...
+                      <Button onClick={() => toggleDescription(podcast.id)}>
+                        Read More
+                      </Button>
+                    </>
+                  )}
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  Seasons: {podcast.seasons}
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  Updated: {podcast.updated}
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  Genres:{" "}
+                  {podcast.genres.map((genre) => genres[genre]).join(", ")}
+                </Typography>
+              </CardContent>
+            </Card>
+          </Container>
+        ))}
+      </Slider>
+      <TextField
+        label="Filter Podcasts"
+        variant="outlined"
+        // fullWidth
+        margin="normal"
+        value={filter}
+        onChange={(e) => setFilter(e.target.value)}
+        sx={style.infoCardContainerFilterBox}
+      />
+      <Grid container spacing={2} sx={style.infoCardContainerGridContainer}>
+        {filteredData.map((podcast) => (
+          <Grid item xs={12} sm={6} md={4} key={podcast.id}>
+            <Card style={{ height: "100%" }}>
+              <CardMedia
+                component="img"
+                image={podcast.image}
+                alt={podcast.title}
+              />
+              <CardContent>
+                <Typography variant="h5" component="div">
+                  {podcast.title}
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  {showFullDescription[podcast.id] ? (
+                    <>
+                      {podcast.description}
+                      <Button onClick={() => toggleDescription(podcast.id)}>
+                        Read Less
+                      </Button>
+                    </>
+                  ) : (
+                    <>
+                      {podcast.description.slice(0, 100)}...
+                      <Button onClick={() => toggleDescription(podcast.id)}>
+                        Read More
+                      </Button>
+                    </>
+                  )}
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  Seasons: {podcast.seasons}
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  Updated: {podcast.updated}
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  Genres:{" "}
+                  {podcast.genres.map((genre) => genres[genre]).join(", ")}
+                </Typography>
               </CardContent>
             </Card>
           </Grid>
         ))}
-    </Grid>
+      </Grid>
+    </Container>
   );
 };
 
--- src/feature/content/styles/infoCard-styles.jsx
+++ src/feature/content/styles/infoCard-styles.jsx
@@ -1,7 +1,31 @@
 export const styles = (theme) => {
   return {
-    gridContainer: {
-      width: "25%",
+    infoCardMainContainer: {
+      flex: "wrap",
+      overflowX: "none",
+      width: "100%",
+      overflowX: "hidden",
+      backgroundColor: theme.primary.light,
     },
+    infoCardCarasoulCardContainerSlider: {
+      width: "35%",
+      marginBottom: "1rem",
+      marginLeft: "auto",
+      marginRight: "auto",
+      marginTop: "1rem",
+    },
+    infoCardCarasoulCardContainer: {
+      padding: "1rem",
+      textAlign: "center",
+    },
+    infoCardContainerFilterBox: {
+      marginBottom: "1rem",
+    },
+
+    // infoCardContainerGridContainer: {
+    //   maxWidth: "1200px", // Set a maximum width for the container
+    //   margin: "0 auto", // Center the container horizontally
+    //   padding: "20px", // Add padding to create spacing around the grid
+    // },
   };
 };
