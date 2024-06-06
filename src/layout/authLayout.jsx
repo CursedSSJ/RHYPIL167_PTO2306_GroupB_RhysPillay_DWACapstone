717cca0a7ffa3c98c35ba3c925a26351c143d015
Rhys Pillay <rhys.pillay@gmail.com>
2024-06-06 02:17:35 +0200

    more changes

--- src/layout/authLayout.jsx
+++ src/layout/authLayout.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import { Container } from "@mui/material";
-import styles from "./styles/authLayout-styles";
+import styles from "./styles/layoutContent-styles";
 import { useTheme } from "@mui/material/styles";
 
 const LoginPage = () => {
   const theme = useTheme();
   const style = styles(theme);
   return (
-    <Container sx={style.mainContainer}>
+    <Container sx={style.mainContainerAuth}>
       <Container sx={style.loginLeftDiv}>
         <img
           src="../public/assets/Login-Image.png"
--- src/layout/styles/layoutContent-styles.jsx
+++ src/layout/styles/layoutContent-styles.jsx
@@ -18,6 +18,41 @@ const styles = (theme) => {
       flexDirection: "column",
       minWidth: "100% ",
     },
+    mainContainerAuth: {
+      margin: "0px",
+      padding: "0px !important",
+      minWidth: "100%",
+      display: "inline-flex",
+      justifyContent: "center",
+      alignItems: "center",
+      height: "100vh",
+      "& .MuiContainer-root": {
+        maxWidth: "100vw",
+        padding: "0px",
+      },
+    },
+    loginLeftDiv: {
+      margin: "0px",
+      padding: "0px !important",
+      width: "40%",
+      height: "100%",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      backgroundColor: "#000a13",
+    },
+    loginLogo: {
+      width: "90%",
+      height: "90%",
+    },
+    loginRightDiv: {
+      width: "60%",
+      height: "100%",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      backgroundColor: theme.primary.light,
+    },
   };
 };
 
